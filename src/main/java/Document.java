669a31206fc60c35cae7930c3aa855c5941d2d72
João Silva <joaosilva@revtut.net>
2017-03-30 18:52:51 +0100

    Merged conflicts.

--- src/main/java/Document.java
+++ src/main/java/Document.java
@@ -1,13 +1,23 @@
+import sun.plugin.dom.exception.InvalidStateException;
+
+import java.io.InvalidClassException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
 import java.io.Serializable;
 
-public class Document implements Serializable {
-    private List<Shape> shapes = new ArrayList<>();
-    private Stack<Command> commands = new Stack<>();
-    private Stack<Command> undos = new Stack<>();
+public class Document {
+    private List<Shape> shapes;
+    private LinkedList<Command> commands;
+    private int commandIndex;
+
+    public Document() {
+        shapes = new ArrayList<>();
+        commands = new LinkedList<>();
+        commandIndex = -1;
+    }
 
     public void addShape(Shape shape) {
         shapes.add(shape);
@@ -25,17 +35,42 @@ public class Document implements Serializable {
     }
 
     public void execute(Command cmd) {
-        commands.push(cmd);
+        commands.add(++commandIndex, cmd);
         cmd.execute();
     }
 
     public void undo() {
-        Command lastCommand = commands.pop();
-        undos.push(lastCommand);
+        if(commandIndex < 0)
+            throw new InvalidStateException("Nothing to undo...");
+
+        Command lastCommand = commands.get(commandIndex);
+        commandIndex--;
         lastCommand.undo();
     }
 
     public void redo() {
-        execute(undos.pop());
+        if(commandIndex + 1 >= commands.size())
+            throw new InvalidStateException("Nothing to redo...");
+
+        commandIndex++;
+        Command nextCommand = commands.get(commandIndex);
+        if(!isCommandValid(nextCommand))
+            throw new InvalidStateException("Can not apply transformations over deleted shape...");
+
+        nextCommand.execute();
+    }
+
+    public void redoAll() {
+        for(int i = commandIndex; i < commands.size() - 1; i++) {
+            redo();
+        }
+    }
+
+    private boolean isCommandValid(Command cmd) {
+        if(cmd instanceof TransformShapeCommand) {
+            if (!shapes.contains(((TransformShapeCommand) cmd).getShape()))
+                return false;
+        }
+        return true;
     }
 }
---
+++ src/main/java/TransformShapeCommand.java
@@ -0,0 +1,5 @@
+/**
+ * Created by joaos on 30/03/2017.
+ */
+public abstract class TransformShapeCommand extends ShapeCommand {
+}
