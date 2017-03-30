6d19d6c53aa8a01fe52b3f670daf55f170a6019b
João Bernardino <joao.mnb@gmail.com>
2017-03-30 11:29:44 +0100

    Add CreatePolygon Command

---
+++ test/main/CreatePolygonTest.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreatePolygonTest
+{
+    @Test
+    public void createShape() throws Exception {
+        Document d1 = new Document();
+        d1.execute(new CreatePolygon(d1, 100, 200));
+        assertEquals(1, d1.getShapes().size());
+    }
+
+}
